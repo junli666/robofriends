54e116b972241a91e7d4aefb373143067ef34461
Jun <junli666@gmail.com>
2023-02-04 15:58:40 +1100

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
-import { robots } from "./robots.js";
+
+import "./App.css";
 export default function App() {
-  const initialState = {
-    robots: robots,
-    searchField: "bc",
-  };
-  const [state, setState] = useState(initialState);
+  const [state, setState] = useState({
+    robots: [],
+    searchField: "",
+  });
+  const [initialState, setInitialStateState] = useState({
+    robots: [],
+    searchField: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((users) => {
+          setState({ ...state, robots: users });
+          setInitialStateState({ ...state, robots: users });
+        });
+    })();
+  });
   const handleSearchChange = (value) => {
-    const filteredRobots = robots.filter((a) =>
+    const filteredRobots = initialState.robots.filter((a) =>
       a.name.toLowerCase().includes(value.toLowerCase())
     );
     setState({ robots: filteredRobots, searchField: value });
   };
 
-  return (
-    <div className="tc">
-      <h1>RoboFriends</h1>
-      <SearchBox
-        searchField={state.searchField}
-        searchChange={handleSearchChange}
-      />
-      <CardList robots={state.robots} />
-    </div>
-  );
+  if (state && state.robots.length)
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox
+          searchField={state.searchField}
+          searchChange={handleSearchChange}
+        />
+        <CardList robots={state.robots} />
+      </div>
+    );
+  else return <h1>Loading</h1>;
 }
